5f3409e38d916d4dbebeb170e0dea146de9f48d4
lucaayscough <lucaayscough@gmail.com>
2025-01-28 14:12:30 +0000

    A bit of tidying up.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,5 +1,4 @@
 #include "plugin.hpp"
-#include "editor.hpp"
 
 #define DEFAULT_BUSES juce::AudioProcessor::BusesProperties() \
   .withInput("Input", juce::AudioChannelSet::stereo()) \
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "editor.hpp"
 #include <juce_audio_processors/juce_audio_processors.h>
 
 struct Plugin : juce::AudioProcessor {
